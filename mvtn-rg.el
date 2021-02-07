647fc37fcd94c71070b10fb929dc3a9248f5d1e1
Toerd@archlinux <philipg@posteo.de>
2021-02-07 21:13:37 +0100

    feat: Added support for rg (ripgrep) search in mvtn

---
+++ mvtn-rg.el
@@ -0,0 +1,34 @@
+;;; mvtn-rg.el --- Support for rg (ripgrep) for mvtn -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; This file may be loaded in addition to mvtn's core (mvtn.el) to add support
+;; for rg (ripgrep) (https://github.com/BurntSushi/ripgrep)
+;; using rg.el (https://github.com/dajva/rg.el).
+
+;;; Code:
+
+(require 'mvtn)
+
+(declare-function rg "ext:rg")
+(defvar rg-executable)
+
+;;;###autoload
+(defun mvtn-search-full-text-rg (string exclude-dirs)
+  "Searches STRING using `rg' in `default-directory',
+excluding directories specified as a list of strings in
+DIRS. Fall back to `mvtn-search-full-text--grep' when
+`rg-executable' is not found."
+  (require 'rg)
+  (if (not (executable-find rg-executable))
+      (mvtn-search-full-text-grep string exclude-dirs)
+    (let ((rg-command-line-flags
+           (mapcar
+            (lambda (dir) (format "-g=!%s" dir))
+            exclude-dirs)))
+    (rg string "everything" default-directory))))
+
+
+(provide 'mvtn-rg)
+
+;;; mvtn-rg.el ends here
