eee4e3eeb71d6c0ec25f6e7dcfb6811d53feb196
f1rstperson <f1p@posteo.de>
2021-08-26 22:51:56 +0200

    delete file import functions
    I simply never encountered a practical use case for these. If adding foreign
    files to mvtn really is needed in some niche cases, one could always do so
    manually (which simply means copying/moving the file into the desired directory
    and prepending a timestamp to the filename).

--- mvtn-file-helpers.el
+++ mvtn-file-helpers.el
@@ -39,45 +39,6 @@ first occurence of \"title: \"."
     (insert name)
     (save-buffer)))
 
-;;;###autoload
-(defun mvtn-import-file (filename title tags &optional move)
-  "Import the file given by FILENAME into
-`mvtn-get-create-current-year-directory' with the new filename
-given by `mvtn-generate-file-name'. By default, the file is
-copied. If MOVE is non-nil, the file is instead moved and the
-buffer visiting the file is pointed to the new direction - if
-such a buffer exists."
-  (interactive (let* ((filename (read-file-name "Filename: "))
-                      (title (read-string
-                              "Title: " (file-name-sans-extension
-                                         (file-name-nondirectory filename))))
-                      (tags (read-string "Tags: ")))
-                 (list filename title tags)))
-  (let* ((ext (or (file-name-extension filename) "txt"))
-         (mvtn-name (mvtn-generate-file-name
-                     (mvtn-current-timestamp 'second) title ext tags)))
-    (if move
-        (progn (rename-file filename mvtn-name)
-               (when (buffer-live-p (find-buffer-visiting filename))
-                 (with-current-buffer (find-buffer-visiting filename)
-                   (rename-buffer mvtn-name)
-                   (set-visited-file-name mvtn-name))))
-      (copy-file filename mvtn-name))
-    (message "Imported %s into %s" filename mvtn-name)))
-
-;;;###autoload
-(defun mvtn-import-file-dired ()
-  "Use `dired-get-filename' to populate the FILENAME argument of
-`mvtn-import-file' (which see for further details)."
-  (interactive)
-  (require 'dired)
-  (let* ((filename (dired-get-filename))
-         (title (read-string "Title: " (file-name-sans-extension
-                                        (file-name-nondirectory filename))))
-         (tags (read-string "Tags: "))
-         (move (not (yes-or-no-p "Keep original file?"))))
-    (mvtn-import-file filename title tags move)))
-
 (provide 'mvtn-file-helpers)
 
 ;;; mvtn-file-helpers.el ends here
